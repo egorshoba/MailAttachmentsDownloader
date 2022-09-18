3c73b2df500cce2bb9e06eb4345e975460a8cf15
egorshoba <fobos@fastmail.com>
2022-09-18 16:31:41 +0500

    Simple .jpg downloader has been implemented

---
+++ Config.cs
@@ -0,0 +1,8 @@
+public class Config
+{
+    public string Email {get;set;}
+    public string Password {get;set;}
+    public string ImapServer {get;set;}
+    public int ImapPort{get;set;}
+    public string FolderName {get;set;}
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using MailKit.Net.Imap;
+using MimeKit;
+using MailKit;
+using MailKit.Search;
+using MailKit.Security;
+
+var configText = File.ReadAllText("appsettings.json");
+System.Console.WriteLine(configText);
+var config = JsonSerializer.Deserialize<Config>(configText);
+if (config is null)
+    throw new NullReferenceException("config is null");
+
+using (var client = new ImapClient())
+{
+    client.Connect(config.ImapServer, config.ImapPort, SecureSocketOptions.SslOnConnect);
+
+    client.Authenticate(config.Email, config.Password);
+
+    var folder = client.GetFolder(config.FolderName);
+    folder.Open(FolderAccess.ReadOnly);
+
+    var uids = folder.Search(SearchQuery.All);
+
+    var items = folder.Fetch(uids, MessageSummaryItems.UniqueId | MessageSummaryItems.BodyStructure)
+    .OrderBy(m=>m.UniqueId);
+    int i = 0;
+    foreach (var item in items)
+    {
+        foreach (var attachment in item.BodyParts)
+        {
+            var entity = folder.GetBodyPart(item.UniqueId, attachment);
+            var part = (MimePart)entity;
+            if (part.ContentType.MimeType is null 
+            || !part.ContentType.MimeType.Contains("image"))
+                continue;
+            System.Console.WriteLine(i);
+            var date = item.Date.ToString();
+            using (var stream = File.Create("images/" + i + ".jpg"))
+                part.Content.DecodeTo(stream);
+            i++;
+        }
+    }
+    client.Disconnect(true);
+}
